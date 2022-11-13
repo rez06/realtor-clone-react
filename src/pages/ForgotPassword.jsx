8287bb5a7514b7626f95ab0cb2333eab4a86454e
rez06 <pelobello.rezyl@gmail.com>
2022-11-13 21:48:53 +0800

    Completed the forgot-password page functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,12 +1,25 @@
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import React from 'react'
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import OAuth from '../Components/OAuth';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
   function onChange(e) {
     setEmail(e.target.value);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Email notification sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
   return (
     <div>
@@ -17,7 +30,7 @@ export default function ForgotPassword() {
             <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60" alt="key" className='w-full rounded-2xl' />
           </div>
           <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-            <form>
+            <form onSubmit={onSubmit}>
               <input
                 type="email"
                 id="email"
